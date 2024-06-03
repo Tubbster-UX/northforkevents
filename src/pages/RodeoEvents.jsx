457c4173134bd84987ca13666dd2bf0713ff3127
Alexavier Brink <65963015+Tubbster-UX@users.noreply.github.com>
2024-06-02 22:48:35 -0600

    chore: Adjust grid and styling for RodeoEvents page
    The commit updates the grid layout and adds responsive styling to the event cards on the RodeoEvents page.

--- src/pages/RodeoEvents.jsx
+++ src/pages/RodeoEvents.jsx
@@ -9,7 +9,7 @@ const RodeoEvents = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
-    const [eventsPerPage] = useState(4);
+    const [eventsPerPage] = useState(3);
 
     useEffect(() => {
         fetchEvents();
@@ -66,11 +66,11 @@ const RodeoEvents = () => {
                 <Alert message={error} type="error" />
             ) : (
                 <>
-<div id='events' className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto md:px-4 py-14 w-full h-full place-items-center">
-    {currentEvents.map((event, index) => (
-        <EventCard event={event} className="w-full" />
-    ))}
-</div>
+                    <div id='events' className="grid grid-cols-1 p-2 md:grid-cols-3 gap-4 mx-auto py-14 w-full h-full place-items-center">
+                        {currentEvents.map((event, index) => (
+                            <EventCard event={event} />
+                        ))}
+                    </div>
                     <div className="flex justify-center mt-4">
                         <Pagination
                             currentPage={currentPage}
