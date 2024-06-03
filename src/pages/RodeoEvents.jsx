204f629d74518d71b9dfeb986b0267ddee976b89
Tubbster-UX <Alexavierbrink@gmail.com>
2024-06-03 08:56:33 -0600

    chore: Update RodeoEvents page layout and styling
    Adjust the grid layout and add responsive styling to the event cards on the RodeoEvents page.

--- src/pages/RodeoEvents.jsx
+++ src/pages/RodeoEvents.jsx
@@ -76,13 +76,9 @@ const RodeoEvents = () => {
                 </div>
             ) : (
                 <>
-<<<<<<< Updated upstream
-                    <div id='events' className="grid grid-cols-1 p-2 md:grid-cols-3 gap-4 mx-auto py-14 w-full h-full place-items-center">
-=======
                     <div id='events' className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto md:px-4 py-14 w-full h-full place-items-center">
->>>>>>> Stashed changes
                         {currentEvents.map((event, index) => (
-                            <EventCard event={event} />
+                            <EventCard event={event} className="w-full" />
                         ))}
                     </div>
                     <div className="flex justify-center mt-4">
